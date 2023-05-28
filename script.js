087679aff990c6ac0cf44674cf7eb72b8d6630a2
jeromemorro <106542817+jeromemorro@users.noreply.github.com>
2023-05-27 20:51:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const words = ["January", "February", "March", "April", "May", "June", "July", "
 
 const wordContainer = document.getElementById("word");
 const generateButton = document.getElementById("generate-button");
+const numberInput = document.getElementById("number-input");
 
 generateButton.addEventListener("click", () => {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  const randomWord = words[randomIndex];
-  wordContainer.textContent = randomWord;
+  const selectedNumber = parseInt(numberInput.value);
+  
+  if (selectedNumber >= 1 && selectedNumber <= 30) {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const randomWord = words[randomIndex];
+    wordContainer.textContent = randomWord;
+  } else {
+    wordContainer.textContent = "Please enter a valid number (1-30)";
+  }
 });
