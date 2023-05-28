755f6b523664c354878a52be95a80eb859112c20
jeromemorro <106542817+jeromemorro@users.noreply.github.com>
2023-05-28 04:04:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ window.addEventListener('DOMContentLoaded', function() {
   var wordElement = document.getElementById('word');
   var secondsSelect = document.getElementById('seconds-select');
   var intervalId;
+  var startStopBtn = document.getElementById("start-stop-btn");
+  
+  startStopBtn.addEventListener("click", function() {
+  if (startStopBtn.classList.contains("start")) {
+    startStopBtn.textContent = "Stop";
+    startStopBtn.classList.remove("start");
+    startStopBtn.classList.add("stop");
+  } 
+  else {
+    startStopBtn.textContent = "Start";
+    startStopBtn.classList.remove("stop");
+    startStopBtn.classList.add("start");
+  }
+
 
   function generateRandomWord() {
     var words = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'Spring', 'Summer', 'Autumn', 'Winter'];
