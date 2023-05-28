892f905ada3d29cb9b3f05124d069db265324e31
jeromemorro <106542817+jeromemorro@users.noreply.github.com>
2023-05-27 21:16:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const words = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Spring", "Summer", "Autumn", "Winter"];
 
 const wordContainer = document.getElementById("word");
-const generateButton = document.getElementById("generate-button");
 const numberSelect = document.getElementById("number-select");
 
-generateButton.addEventListener("click", () => {
+let intervalId;
+
+numberSelect.addEventListener("change", () => {
   const selectedNumber = parseInt(numberSelect.value);
   
   if (selectedNumber >= 1 && selectedNumber <= 30) {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    const randomWord = words[randomIndex];
-    wordContainer.textContent = randomWord;
+    clearInterval(intervalId);
+    startWordCycle(selectedNumber);
   } else {
     wordContainer.textContent = "Please select a valid number (1-30)";
+    clearInterval(intervalId);
   }
 });
+
+function startWordCycle(seconds) {
+  let index = 0;
+  
+  intervalId = setInterval(() => {
+    const word = words[index];
+    wordContainer.textContent = word;
+    
+    index = (index + 1) % words.length;
+  }, seconds * 1000);
+}
