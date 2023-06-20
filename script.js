f67d46a3d09ef4ba4cb9242859dcb132f79f0acb
jeromemorro <106542817+jeromemorro@users.noreply.github.com>
2023-06-20 00:47:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -366,8 +366,6 @@ window.addEventListener('DOMContentLoaded', function() {
       document.documentElement.requestFullscreen();
     } else if (document.documentElement.mozRequestFullScreen) {
       document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) {
-      document.documentElement.webkitRequestFullscreen();
     } else if (document.documentElement.msRequestFullscreen) {
       document.documentElement.msRequestFullscreen();
     } else if (document.documentElement.webkitEnterFullscreen) {
